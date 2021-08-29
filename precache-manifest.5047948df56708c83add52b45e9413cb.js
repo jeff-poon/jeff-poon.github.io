6d2e280428b881a7a2f0b455cdee971bd883c2e9
Jeff Poon <poonlongsing@gmail.com>
2021-08-30 00:19:13 +0800

    Updates

---
+++ precache-manifest.5047948df56708c83add52b45e9413cb.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4cd14883ea190304488e83fffc60ff3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f7495a16e2da31d09a5b",
+    "url": "/static/css/main.afe79e66.chunk.css"
+  },
+  {
+    "revision": "778941a4c2d851e61532",
+    "url": "/static/js/2.ec292aae.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.ec292aae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7495a16e2da31d09a5b",
+    "url": "/static/js/main.01be754b.chunk.js"
+  },
+  {
+    "revision": "6049462cd925d98c0814",
+    "url": "/static/js/runtime-main.506a126a.js"
+  },
+  {
+    "revision": "608157f3ff1e1741221d5870dbf3cba2",
+    "url": "/static/media/IMG_9564.608157f3.jpg"
+  },
+  {
+    "revision": "b406b059551ff93fa3b61beaa1c6a320",
+    "url": "/static/media/bea.b406b059.png"
+  },
+  {
+    "revision": "32f6b574eb11519020e24620a38bdf55",
+    "url": "/static/media/hashirshoaeb.32f6b574.png"
+  },
+  {
+    "revision": "9d3da0e22086c40d5e3106d01c0f6402",
+    "url": "/static/media/resume.9d3da0e2.pdf"
+  },
+  {
+    "revision": "49a5f4c68732e65eb2a5ac409cf1cadc",
+    "url": "/static/media/ricoh.49a5f4c6.jpg"
+  }
+]);
